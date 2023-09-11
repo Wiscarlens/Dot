b215e8e8e37c6f08f73937e6501406d8449c77e0
Wiscarlens <wiscarlens@gmail.com>
2023-09-10 20:14:20 -0400

    Print ReceiptFragment createPDF method does not work

--- app/src/main/java/com/example/chezelisma/HomeFragment.java
+++ app/src/main/java/com/example/chezelisma/HomeFragment.java
@@ -19,7 +19,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
--- app/src/main/java/com/example/chezelisma/ReceiptFragment.java
+++ app/src/main/java/com/example/chezelisma/ReceiptFragment.java
@@ -2,15 +2,24 @@ package com.example.chezelisma;
 
 import static com.example.chezelisma.LocalFormat.getCurrencyFormat;
 
+
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +27,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 
 public class ReceiptFragment extends Fragment {
+    final static int REQUEST_CODE = 1232;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,22 +107,89 @@ public class ReceiptFragment extends Fragment {
                 }
         );
 
+        askPermission();
+
 
         // When user clicks on email button, send email to user with receipt
         emailButton.setOnClickListener(v -> Toast.makeText(getContext(), "Email Button Works", Toast.LENGTH_SHORT).show());
 
         // When user clicks on print button, print receipt
-        printButton.setOnClickListener(v -> Toast.makeText(getContext(), "Print Button Works", Toast.LENGTH_SHORT).show());
+        printButton.setOnClickListener(v -> {
+                createPDF();
+                Toast.makeText(getContext(), "Print Button Works", Toast.LENGTH_SHORT).show();
+            }
+
+        );
 
         // When user clicks on share button, share receipt
         shareButton.setOnClickListener(v -> Toast.makeText(getContext(), "Share Button Works", Toast.LENGTH_SHORT).show());
 
     }
 
+    private void createPDF() {
+        if(ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_receipt, null);
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                getActivity().getDisplay().getRealMetrics(displayMetrics);
+            } else {
+                getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            }
+
+            view.measure(View.MeasureSpec.makeMeasureSpec(displayMetrics.widthPixels, View.MeasureSpec.EXACTLY),
+                    View.MeasureSpec.makeMeasureSpec(displayMetrics.heightPixels, View.MeasureSpec.EXACTLY));
+
+            view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+
+            PdfDocument document = new PdfDocument();
+
+            int viewWidth = view.getMeasuredWidth();
+            int viewHeight = view.getMeasuredHeight();
+
+            PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1080, 1920, 1).create();
+            PdfDocument.Page page = document.startPage(pageInfo);
+
+            Canvas canvas = page.getCanvas();
+            view.draw(canvas);
+
+            document.finishPage(page);
+
+
+            File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            String fileName = "exampleXML.pdf";
+            File file = new File(downloadsDir, fileName);
+
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                document.writeTo(fos);
+                document.close();
+                fos.close();
+                Toast.makeText(getContext(), "Converted to PDF Successfully!!!", Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                Log.d("myLog", "File not found exception" + e.getMessage());
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            Toast.makeText(getContext(), "Permission Denied!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_receipt, container, false);
+    }
+
+    private void askPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+        }, REQUEST_CODE);
     }
 
 }
